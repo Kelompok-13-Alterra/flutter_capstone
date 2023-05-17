8c07c548f30b1a73e745442c7e0af1c3ab3da82a
FarighMAkmalullah <48850497+FarighMAkmalullah@users.noreply.github.com>
2023-05-17 10:13:54 +0700

    memperbaiki folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_capstone/screen/login-screen.dart';
+import 'package:flutter_capstone/screens/login-screen.dart';
 
 void main() {
   runApp(const MyApp());
