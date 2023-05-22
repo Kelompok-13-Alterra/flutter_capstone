ec1db312882422356ba0303d8278a1fae3e9f225
pteryuu <102342752+pteryuu@users.noreply.github.com>
2023-05-22 20:47:57 +0700

    replaced and seperated widget and screen

--- lib/widgets/profile/card_profile.dart
+++ lib/widgets/profile/card_profile.dart
@@ -11,51 +11,54 @@ Widget cardProfile(BuildContext context) {
     child: Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        SizedBox(
-          height: 136,
-          width: 156,
-          child: Card(
-            color: const Color(0xFFF2F6FF),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(
-                top: 16,
-                left: 16,
-                bottom: 12,
+        Expanded(
+          flex: 1,
+          child: SizedBox(
+            height: 136,
+            width: 156,
+            child: Card(
+              color: const Color(0xFFF2F6FF),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image.asset(
-                    "assets/image/Image_total_kantor.png",
-                    height: 24,
-                    width: 24,
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  ),
-                  Text(
-                    "Total Kantor",
-                    style: TextTotalKantor().text21,
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  ),
-                  Text(
-                    "yang kamu kunjungi",
-                    style: TextAbuabu().text22,
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  ),
-                  Text(
-                    "36",
-                    style: TextTotal().text23,
-                  ),
-                ],
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 16,
+                  left: 16,
+                  bottom: 12,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image.asset(
+                      "assets/image/Image_total_kantor.png",
+                      height: 24,
+                      width: 24,
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      "Total Kantor",
+                      style: TextTotalKantor().text21,
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      "yang kamu kunjungi",
+                      style: TextAbuabu().text22,
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      "36",
+                      style: TextTotal().text23,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
@@ -63,51 +66,54 @@ Widget cardProfile(BuildContext context) {
         // const SizedBox(
         //   width: 16,
         // ),
-        SizedBox(
-          height: 136,
-          width: 156,
-          child: Card(
-            color: const Color(0xFFFFFCF5),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(
-                top: 16,
-                left: 16,
-                bottom: 12,
+        Expanded(
+          flex: 1,
+          child: SizedBox(
+            height: 136,
+            width: 156,
+            child: Card(
+              color: const Color(0xFFFFFCF5),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
               ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Image.asset(
-                    "assets/image/Image_total_co-working.png",
-                    height: 24,
-                    width: 24,
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  ),
-                  Text(
-                    "Total Kantor",
-                    style: TextTotalKantor().text21,
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  ),
-                  Text(
-                    "yang kamu kunjungi",
-                    style: TextAbuabu().text22,
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  ),
-                  Text(
-                    "36",
-                    style: TextTotal().text23,
-                  ),
-                ],
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 16,
+                  left: 16,
+                  bottom: 12,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image.asset(
+                      "assets/image/Image_total_co-working.png",
+                      height: 24,
+                      width: 24,
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      "Total Kantor",
+                      style: TextTotalKantor().text21,
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      "yang kamu kunjungi",
+                      style: TextAbuabu().text22,
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      "36",
+                      style: TextTotal().text23,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
