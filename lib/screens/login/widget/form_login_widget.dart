3d43e40b25730112834c7c728cc25ec34df099c7
pteryuu <102342752+pteryuu@users.noreply.github.com>
2023-05-24 17:28:33 +0700

    delete useless file and code

--- lib/screens/login/widget/form_login_widget.dart
+++ lib/screens/login/widget/form_login_widget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_capstone/widgets/login/text_or_widget.dart';
+import 'package:flutter_capstone/screens/login/widget/text_or_widget.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:email_validator/email_validator.dart';
