61d6980da71fb8c7c9a755cabf47975b0efd08d2
FarighMAkmalullah <48850497+FarighMAkmalullah@users.noreply.github.com>
2023-06-01 13:32:48 +0700

    Form awalan

--- lib/screens/edit_profile/editprofile-screen.dart
+++ lib/screens/edit_profile/editprofile-screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_capstone/style/text_style.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class EditProfile extends StatefulWidget {
   const EditProfile({super.key});
@@ -9,6 +10,12 @@ class EditProfile extends StatefulWidget {
 }
 
 class _EditProfileState extends State<EditProfile> {
+  final formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _companyController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  String? selectedValue;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -66,11 +73,133 @@ class _EditProfileState extends State<EditProfile> {
                           fontSize: 14,
                         ),
                       ),
-                    )
+                    ),
                   ],
                 ),
               ),
             ),
+            Form(
+              key: formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextFormField(
+                    controller: _nameController,
+                    decoration: InputDecoration(
+                      labelText: 'Nama',
+                      hintStyle: GoogleFonts.roboto(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16,
+                      ),
+                      hintText: 'Input Full Name',
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      border: const OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 25.0),
+                  TextFormField(
+                    controller: _companyController,
+                    decoration: InputDecoration(
+                      labelText: 'Company',
+                      hintStyle: GoogleFonts.roboto(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16,
+                      ),
+                      hintText: 'Input Company Name',
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      border: const OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 25.0),
+                  TextFormField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      hintStyle: GoogleFonts.roboto(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16,
+                      ),
+                      hintText: 'Input Email',
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      border: const OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 25.0),
+                  TextFormField(
+                    controller: _passwordController,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      hintStyle: GoogleFonts.roboto(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 16,
+                      ),
+                      hintText: 'Input Password',
+                      floatingLabelBehavior: FloatingLabelBehavior.always,
+                      border: const OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 25.0),
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 24),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          "Gender",
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Row(
+                              children: [
+                                Radio<String>(
+                                  value: "Male",
+                                  groupValue: selectedValue,
+                                  onChanged: (String? value) {
+                                    setState(() {
+                                      selectedValue = value;
+                                    });
+                                  },
+                                ),
+                                const Text('Male'),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Radio<String>(
+                                  value: "Female",
+                                  groupValue: selectedValue,
+                                  onChanged: (String? value) {
+                                    setState(() {
+                                      selectedValue = value;
+                                    });
+                                  },
+                                ),
+                                const Text('Female'),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Radio<String>(
+                                  value: "Other",
+                                  groupValue: selectedValue,
+                                  onChanged: (String? value) {
+                                    setState(() {
+                                      selectedValue = value;
+                                    });
+                                  },
+                                ),
+                                const Text('Other'),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
--- lib/screens/login/widget/form_login_widget.dart
+++ lib/screens/login/widget/form_login_widget.dart
@@ -54,168 +54,166 @@ class _FormLoginState extends State<FormLogin> {
   @override
   Widget build(BuildContext context) {
     return Form(
-        key: formKey,
-        autovalidateMode: AutovalidateMode.onUserInteraction,
-        child: Column(
-          children: [
-            // Form Login
-            //========================================================
-            // Input Email
-            //========================================================
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              controller: _emailController,
-              validator: (email) {
-                if (email != null && !EmailValidator.validate(email)) {
-                  return 'Enter a valid email';
-                } else {
-                  return null;
-                }
-              },
-              decoration: InputDecoration(
-                labelText: 'Email',
-                hintStyle: GoogleFonts.roboto(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 16,
-                ),
-                hintText: 'Input Email',
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-                border: const OutlineInputBorder(),
+      key: formKey,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      child: Column(
+        children: [
+          // Form Login
+          //========================================================
+          // Input Email
+          //========================================================
+          TextFormField(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            controller: _emailController,
+            validator: (email) {
+              if (email != null && !EmailValidator.validate(email)) {
+                return 'Enter a valid email';
+              } else {
+                return null;
+              }
+            },
+            decoration: InputDecoration(
+              labelText: 'Email',
+              hintStyle: GoogleFonts.roboto(
+                fontWeight: FontWeight.w400,
+                fontSize: 16,
               ),
+              hintText: 'Input Email',
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              border: const OutlineInputBorder(),
             ),
-            const SizedBox(height: 25.0),
-            // Input Password
-            //========================================================
-            TextFormField(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              obscureText: _obscureText,
-              controller: _passwordController,
-              validator: (value) {
-                if (value != null && value.length < 5) {
-                  return 'Enter min. 5 characters';
-                } else {
-                  return null;
-                }
-              },
-              decoration: InputDecoration(
-                labelText: 'Password',
-                hintStyle: GoogleFonts.roboto(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 16,
-                ),
-                suffixIcon: IconButton(
-                  icon: Icon(
-                    _obscureText ? Icons.visibility_off : Icons.visibility,
-                  ),
-                  onPressed: _togglePasswordVisibility,
-                ),
-                hintText: 'Input Password',
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-                border: const OutlineInputBorder(),
+          ),
+          const SizedBox(height: 25.0),
+          // Input Password
+          //========================================================
+          TextFormField(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            obscureText: _obscureText,
+            controller: _passwordController,
+            validator: (value) {
+              if (value != null && value.length < 5) {
+                return 'Enter min. 5 characters';
+              } else {
+                return null;
+              }
+            },
+            decoration: InputDecoration(
+              labelText: 'Password',
+              hintStyle: GoogleFonts.roboto(
+                fontWeight: FontWeight.w400,
+                fontSize: 16,
               ),
+              suffixIcon: IconButton(
+                icon: Icon(
+                  _obscureText ? Icons.visibility_off : Icons.visibility,
+                ),
+                onPressed: _togglePasswordVisibility,
+              ),
+              hintText: 'Input Password',
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              border: const OutlineInputBorder(),
             ),
-            const SizedBox(height: 20.0),
+          ),
+          const SizedBox(height: 20.0),
 
-            // CheckBox remember me
-            //========================================================
-            Row(
-              children: <Widget>[
-                SizedBox(
-                  width: 24,
-                  child: Checkbox(
-                    value: isChecked,
-                    onChanged: (value) {
-                      setState(() {
-                        isChecked = value!;
-                      });
-                    },
-                  ),
+          // CheckBox remember me
+          //========================================================
+          Row(
+            children: <Widget>[
+              SizedBox(
+                width: 24,
+                child: Checkbox(
+                  value: isChecked,
+                  onChanged: (value) {
+                    setState(() {
+                      isChecked = value!;
+                    });
+                  },
                 ),
-                const SizedBox(
-                  width: 7,
+              ),
+              const SizedBox(
+                width: 7,
+              ),
+              Text(
+                'Remember me',
+                style: GoogleFonts.roboto(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 14,
                 ),
-                Text(
-                  'Remember me',
-                  style: GoogleFonts.roboto(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 14,
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 25.0),
+              ),
+            ],
+          ),
+          const SizedBox(height: 25.0),
 
-            // Button Login
-            //========================================================
-            FractionallySizedBox(
-              widthFactor: 1.0,
-              child: SizedBox(
-                height: 48,
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    elevation: MaterialStateProperty.all(0),
-                    backgroundColor:
-                        MaterialStateProperty.all(const Color(0xFF005DB9)),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.circular(100), // Bentuk border
-                      ),
+          // Button Login
+          //========================================================
+          FractionallySizedBox(
+            widthFactor: 1.0,
+            child: SizedBox(
+              height: 48,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  elevation: MaterialStateProperty.all(0),
+                  backgroundColor:
+                      MaterialStateProperty.all(const Color(0xFF005DB9)),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(100), // Bentuk border
                     ),
                   ),
-                  onPressed: _submitLogin,
-                  child: Text(
-                    'Login',
-                    style: GoogleFonts.roboto(),
-                  ),
+                ),
+                onPressed: _submitLogin,
+                child: Text(
+                  'Login',
+                  style: GoogleFonts.roboto(),
                 ),
               ),
             ),
-            const SizedBox(height: 10.0),
+          ),
+          const SizedBox(height: 10.0),
 
-            // Style untuk text OR
-            //========================================================
-            const TextOr(),
+          // Style untuk text OR
+          //========================================================
+          const TextOr(),
 
-            // Button Login
-            //========================================================
-            const SizedBox(height: 10.0),
-            FractionallySizedBox(
-              widthFactor: 1.0,
-              child: SizedBox(
-                height: 48,
-                child: ElevatedButton(
-                  style: ButtonStyle(
-                    elevation: MaterialStateProperty.all(0),
-                    backgroundColor: MaterialStateProperty.all(Colors.white),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        side: const BorderSide(
-                            width: 1, color: Color(0xFF292A2D)),
-                        borderRadius:
-                            BorderRadius.circular(100), // Bentuk border
-                      ),
+          // Button Login
+          //========================================================
+          const SizedBox(height: 10.0),
+          FractionallySizedBox(
+            widthFactor: 1.0,
+            child: SizedBox(
+              height: 48,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  elevation: MaterialStateProperty.all(0),
+                  backgroundColor: MaterialStateProperty.all(Colors.white),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      side:
+                          const BorderSide(width: 1, color: Color(0xFF292A2D)),
+                      borderRadius: BorderRadius.circular(100), // Bentuk border
                     ),
                   ),
-                  onPressed: () {},
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SvgPicture.asset('assets/img/google.svg'),
-                      const SizedBox(
-                        width: 8,
-                      ),
-                      Text(
-                        'Sign In With Google',
-                        style:
-                            GoogleFonts.roboto(color: const Color(0xFF292A2D)),
-                      ),
-                    ],
-                  ),
+                ),
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SvgPicture.asset('assets/img/google.svg'),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      'Sign In With Google',
+                      style: GoogleFonts.roboto(color: const Color(0xFF292A2D)),
+                    ),
+                  ],
                 ),
               ),
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
