2059b1c71fbf9e006e394fdb596f5060ffaecb85
ULFA LADAYYA <ladayaulfa@upi.edu>
2023-06-06 11:28:03 +0700

    Add search office feature
    Add search office feature

---
+++ lib/screens/card-search-bar/search_office_view_model.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_capstone/model/search_office/search_model.dart';
+
+class SearchOfficeViewModel with ChangeNotifier {
+  final TextEditingController _searchBarController = TextEditingController();
+  TextEditingController get searchBarController => _searchBarController;
+  String _searchName = "";
+  String get searchName => _searchName;
+  Datum? model;
+
+  onChange(String value) {
+    if (value.length >= 3) {
+      _searchName = value;
+      notifyListeners();
+    }
+    //else if (value != model?.location) {
+    //   const LocationNotFoundScreen();
+    //   notifyListeners();
+    // }
+  }
+}
